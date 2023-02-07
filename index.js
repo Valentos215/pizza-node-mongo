d58f89add78b3845dbec0b6f3633079ddd621a89
Valentos215 <teskavale46@gmail.com>
2023-02-07 12:23:10 +0200

    Changes for deploy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./app");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9001;
 
 app.listen(port, () => console.log(`Server has been started at port ${port}.`));
